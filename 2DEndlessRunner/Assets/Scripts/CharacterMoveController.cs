5418d7dcbcf4fe9f3412bcba8845ef5872fecf0e
jazyk3895 <andysejati@gmail.com>
2021-04-18 17:20:01 +0700

    Initial Commit. Added Jumping Sound

---
+++ 2DEndlessRunner/Assets/Scripts/CharacterMoveController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveAccel;
+    public float maxSpeed;
+    private Rigidbody2D rig;
+
+    [Header("Jump")]
+    public float jumpAccel;
+
+    private bool isJumping;
+
+    [Header("Ground Raycast")]
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+
+    private bool isOnGround;
+
+    private Animator anim;
+
+    private CharacterSoundController sound;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sound = GetComponent<CharacterSoundController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (isOnGround)
+            {
+                isJumping = true;
+
+                sound.PlayJump();
+            }
+        }
+
+        anim.SetBool("isOnGround", isOnGround);
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundRaycastDistance, groundLayerMask);
+        {
+            if (hit)
+            {
+                if (!isOnGround && rig.velocity.y <= 0)
+                {
+                    isOnGround = true;
+                }
+            }
+            else
+            {
+                isOnGround = false;
+            }
+        }
+
+        Vector2 velocityVector = rig.velocity;
+
+        if (isJumping)
+        {
+            velocityVector.y += jumpAccel;
+            isJumping = false;
+        }
+
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
+
+        rig.velocity = velocityVector;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down * groundRaycastDistance), Color.white);
+    }
+}
---
+++ 2DEndlessRunner/Assets/Scripts/CharacterSoundController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundController : MonoBehaviour
+{
+    public AudioClip jump;
+    private AudioSource audioPlayer;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    public void PlayJump()
+    {
+        audioPlayer.PlayOneShot(jump);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
